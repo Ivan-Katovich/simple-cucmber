631a6bee1b7243841a5c6c8d6a83bf4f4e09a267
Ivan-Katovich <kotovich.ivan@gmail.com>
2018-04-27 11:43:50 +0300

    bp step rm

--- step_definitions/base_page_steps.js
+++ step_definitions/base_page_steps.js
@@ -5,8 +5,5 @@ let BasePage = require('../support/base_page');
 let basePage = new BasePage();
 let expect = require('chai').expect;
 cucumber.defineSupportCode(function({ Then }) {
-    Then(/^Element '(.+)' should be displayed$/, async function(elementName) {
-        let state = await basePage.isElementVisible(elementName);
-        expect(state).to.equal(true);
-    });
+
 });
