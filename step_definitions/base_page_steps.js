441695e1357ef686fdd5aa1dc72a2c113e239373
Ivan-Katovich <kotovich.ivan@gmail.com>
2018-04-27 11:43:06 +0300

    bp step rm

--- step_definitions/base_page_steps.js
+++ step_definitions/base_page_steps.js
@@ -4,10 +4,6 @@ let BasePage = require('../support/base_page');
 let basePage = new BasePage();
 let expect = require('chai').expect;
 cucumber.defineSupportCode(function({ Then }) {
-    Then(/^Element '(.+)' should be displayed$/, async function(elementName) {
-        let state = await basePage.isElementVisible(elementName);
-        expect(state).to.equal(true);
-    });
     Then(/^'(.+)'(?:st|th|nd|rd|) item in '(table|list)' should contain '(.+)' (?:cell|row) with text '(.+)'$/, async function(itemNameOrNumber, elemType, itemPoint, expectedText) {
         itemNameOrNumber = parseInt(itemNameOrNumber) ? itemNameOrNumber -1 : itemNameOrNumber;
         let actualText = await basePage.getItem(elemType, itemNameOrNumber).getElementText(itemPoint);
